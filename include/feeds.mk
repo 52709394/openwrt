70b4e7dff9e01d058c292bd715d101cd28b5a82f
52709394 <186333925+52709394@users.noreply.github.com>
2026-08-23 11:02:05 +0800

    Refactor feed handling in feeds.mk

--- include/feeds.mk
+++ include/feeds.mk
@@ -41,7 +41,8 @@ define FeedSourcesAppendOPKG
 		echo 'src/gz %d_kmods %U/targets/%S/kmods/$(LINUX_VERSION)-$(LINUX_RELEASE)-$(LINUX_VERMAGIC)';) \
 	$(foreach feed,$(FEEDS_AVAILABLE), \
 		$(if $(CONFIG_FEED_$(feed)), \
-			echo '$(if $(filter m,$(CONFIG_FEED_$(feed))),# )src/gz %d_$(feed) %U/packages/%A/$(feed)';)))) \
+            $(if $(filter kenzo small,$(feed)),, \
+				echo '$(if $(filter m,$(CONFIG_FEED_$(feed))),# )src/gz %d_$(feed) %U/packages/%A/$(feed)';))))) \
 ) >> $(1)
 endef
 
@@ -55,8 +56,7 @@ define FeedSourcesAppendAPK
 		echo '%U/targets/%S/kmods/$(LINUX_VERSION)-$(LINUX_RELEASE)-$(LINUX_VERMAGIC)/packages.adb';) \
 	$(foreach feed,$(FEEDS_AVAILABLE), \
 		$(if $(CONFIG_FEED_$(feed)), \
-			$(if $(filter kenzo small,$(feed)),, \
-				echo '$(if $(filter m,$(CONFIG_FEED_$(feed))),# )%U/packages/%A/$(feed)/packages.adb';))))) \
+				echo '$(if $(filter m,$(CONFIG_FEED_$(feed))),# )%U/packages/%A/$(feed)/packages.adb';)))) \
 ) >> $(1)
 endef
 
